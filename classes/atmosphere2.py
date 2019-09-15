6e26c575963855c0a3d45763842baab62dbeb22b
syntax-terr0r <37736612+syntax-terr0r@users.noreply.github.com>
2019-09-16 00:23:08 +1000

    Create atmosphere2.py

---
+++ classes/atmosphere2.py
@@ -0,0 +1,39 @@
+import math
+
+class AtmosphereModel():
+    def __init__(self, planet = None, g_0 = None, T_0 = None, rho_0 = None, molM = None):
+        if planet is not None:
+            g_list = {'Venus': 8.87, 'Earth': 9.807, 'Mars': 3.71, 'Titan': 1.352}
+            T_list = {'Venus': 737, 'Earth': 288, 'Mars': 210, 'Titan': 94}
+            rho_list = {'Venus': 65, 'Earth': 1.217, 'Mars': 0.02, 'Titan': 19.85}
+            molM_list = {'Venus': 0.04345, 'Earth': 0.02897, 'Mars': 0.04334, 'Titan': 0.0274}
+            self.g_0 = g_list[planet]    # Surface gravity
+            self.T_0 = T_list[planet]    # Surface temperature
+            self.rho_0 = rho_list[planet]    # Surface density
+            self.molM = molM_list[planet]    # Molecular weight of air
+        else:
+            self.g_0 = g_0
+            self.T_0 = T_0
+            self.rho_0 = rho_0
+            self.molM = molM
+        
+    def get_H(self, alt):
+        R = 8.31446261815324    # Gas constant
+        T = self.T_0 * math.e**(-alt/200)    # Temperature model based on Earth atmosphere
+        H = R * T / (self.molM * self.g_0)    # Scale height (m)
+        
+        return H
+
+    def get_rho(self, alt):
+        altm = alt * 1000    # km to m
+        H = self.get_H(alt)
+        rho = self.rho_0 * math.e**(-altm/H)    # Density (kg/m^3)
+        
+        return rho
+
+    def get_Q(self, alt, V):
+        rho = self.get_rho(alt)
+        V = V * 1000    # km/s to m/s
+        Q = (rho*(V**2))/2    # Pressure (Pa)
+        
+        return Q
