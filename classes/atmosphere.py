60f0464b615d52898624ddb2cba81d968517ba88
Eero Fadjukoff <eero@fadjukoff.com>
2019-09-15 18:00:40 +0300

    Started using the new atmosphere model for planet.

--- classes/atmosphere.py
+++ classes/atmosphere.py
@@ -1,18 +1,38 @@
-class Atmosphere:
-    """Atmosphere for Planet class."""
-    def __init__(P_0, H):
-        self.P_0 = P_0 #kg/m^3 // density at sea level
-        self.H = H #m, scale height
+import math
+from scipy import constants as con
 
-    def get_P(altitude):
-        """Density (kg/m^3) at altitude (m)"""
-        P = self.P_0 * math.e**(-altitude/self.H)
-        return P
+class AtmosphereModel():
+    """Atmosphere model for the planet class."""
+    def __init__(self, planet = None, g_0 = None, T_0 = None, rho_0 = None, molM = None):
+        if planet is not None:
+            g_list = {'Venus': 8.87, 'Earth': 9.807, 'Mars': 3.71, 'Titan': 1.352}
+            T_list = {'Venus': 737, 'Earth': 288, 'Mars': 210, 'Titan': 94}
+            rho_list = {'Venus': 65, 'Earth': 1.217, 'Mars': 0.02, 'Titan': 19.85}
+            molM_list = {'Venus': 0.04345, 'Earth': 0.02897, 'Mars': 0.04334, 'Titan': 0.0274}
+            self.g_0 = g_list[planet]    # Surface gravity
+            self.T_0 = T_list[planet]    # Surface temperature
+            self.rho_0 = rho_list[planet]    # Surface density
+            self.molM = molM_list[planet]    # Molecular weight of air
+        else:
+            self.g_0 = g_0
+            self.T_0 = T_0
+            self.rho_0 = rho_0
+            self.molM = molM
 
-    def get_Re():
-        return False # TODO: calculate Reynolds number
+    def get_H(self, alt):
+        T = self.T_0 * math.e**(-alt/200)    # Temperature model based on Earth atmosphere
+        H = con.R * T / (self.molM * self.g_0)    # Scale height (m)
 
-    def get_air(altitude):
-        P = self.get_P(altitude)
-        Re = self.get_Re()
-        return {'P':P, 'Re':Re}
+        return H
+
+    def get_rho(self, alt):
+        H = self.get_H(alt)
+        rho = self.rho_0 * math.e**(-altm/H)    # Density (kg/m^3)
+
+        return rho
+
+    def get_Q(self, alt, V):
+        rho = self.get_rho(alt)
+        Q = (rho*(V**2))/2    # Pressure (Pa)
+
+        return Q
--- classes/planet.py
+++ classes/planet.py
@@ -4,18 +4,20 @@ import atmosphere
 
 class Planet:
     """Planet class for space simulations"""
-    def __init__(mass, radius):
+    def __init__(self, mass, radius):
         self.mass = mass
         self.radius = radius
-        self.atmosphere = Atmosphere(1.225, 8400) #numbers for earth
+        self.atmosphere = AtmosphereModel(planet = "Earth") #later numbers from elsewhere
 
-        def get_g(altitude):
-            g = (con.G*self.mass)/(self.radius+altitude)**2
+        def get_g(self, alt):
+            g = (con.G*self.mass)/(self.radius+alt)**2
             return g
 
-        def get_F_gravity(mass, distance):
-            con.G*(()/altitude)
+        def get_F_gravity(self, mass, distance):
+            F = con.G*((self.mass*mass)/distance**2)
+            return F
 
-
-        def get_air(altitude):
-            return self.atmosphere.get_air(altitude)
+        def get_air(self, alt):
+            rho = self.atmosphere.get_rho(alt)
+            air = {'rho':rho}
+            return air
